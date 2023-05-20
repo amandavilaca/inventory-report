5f12c5d6fd5d45d393690f4a8880341699cbc32d
Amanda Vilaça <vilacafontesamanda@gmail.com>
2023-05-20 20:52:51 -0300

    Test the individual product report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    actual_product = Product(
+        id="2",
+        nome_do_produto="fentanyl citrate",
+        nome_da_empresa="Target Corporation",
+        data_de_fabricacao="2020-12-06",
+        data_de_validade="2023-12-25",
+        numero_de_serie="FR29 5951 7573 74OY XKGX 6CSG D20",
+        instrucoes_de_armazenamento="instrucao 2",
+    )
+
+    expected_product = (
+        "O produto fentanyl citrate fabricado em 2020-12-06 "
+        "por Target Corporation com validade até 2023-12-25 "
+        "precisa ser armazenado instrucao 2."
+    )
+
+    assert repr(actual_product) == expected_product
