91bb35cbfc91b4c5e869e1a4a65af4cee2dc4f43
Amanda Vilaça <vilacafontesamanda@gmail.com>
2023-05-20 14:00:25 -0300

    feat: Generate the full version of the report

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,21 @@
-class CompleteReport:
-    pass
+from typing import Dict, List
+from collections import Counter
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(products: List[Dict]) -> str:
+        simple_report = SimpleReport.generate(products)
+
+        companies = [product['nome_da_empresa']
+                     for product in products
+                     if product.get('nome_da_empresa')]
+
+        companies_stock_counts = Counter(companies)
+
+        complete_report = "\nProdutos estocados por empresa:\n"
+        for company, count in companies_stock_counts.items():
+            complete_report += f"- {company}: {count}\n"
+        output = simple_report + complete_report
+        return output
--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -12,25 +12,25 @@ class SimpleReport:
         expiration_dates = list()
         companies = list()
 
-        for product in products:
+        manufacturing_dates = [product['data_de_fabricacao']
+                               for product in products
+                               if product.get('data_de_fabricacao')]
 
-            manufacturing_date = product.get('data_de_fabricacao')
-            expiration_date = product.get('data_de_validade')
-            expiration_date_obj = datetime.strptime(expiration_date,
-                                                    '%Y-%m-%d').date()
-            company = product.get('nome_da_empresa')
+        expiration_dates = [product['data_de_validade']
+                            for product in products
+                            if datetime.strptime(
+            product.get('data_de_validade'), '%Y-%m-%d').date() >= today_date]
 
-            manufacturing_dates.append(manufacturing_date)
-            if expiration_date_obj >= today_date:
-                expiration_dates.append(expiration_date_obj)
-            companies.append(company)
+        companies = [product['nome_da_empresa']
+                     for product in products
+                     if product.get('nome_da_empresa')]
 
-            oldest_date = min(manufacturing_dates, default=None)
-            closest_date = min(expiration_dates, default=None)
-            company_bigger_stock = Counter(companies).most_common(1)[0][0]
+        oldest_date = min(manufacturing_dates, default=None)
+        closest_date = min(expiration_dates, default=None)
+        company_bigger_stock = Counter(companies).most_common(1)[0][0]
 
-            output = f"Data de fabricação mais antiga: {oldest_date}\n"
-            output += f"Data de validade mais próxima: {closest_date}\n"
-            output += f"Empresa com mais produtos: {company_bigger_stock}"
+        output = f"Data de fabricação mais antiga: {oldest_date}\n"
+        output += f"Data de validade mais próxima: {closest_date}\n"
+        output += f"Empresa com mais produtos: {company_bigger_stock}"
 
         return output
